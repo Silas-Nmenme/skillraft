c90ac0aae5b55b1c847c0108578c6538cd01ae8a
Your Name <you@example.com>
2025-12-03 17:21:43 +0100

    `Added dashboard routes and updated app.use() calls`

--- src/index.js
+++ src/index.js
@@ -32,11 +32,13 @@ const authRoutes = require("./routes/auth.routes");
 const founderRoutes = require("./routes/founder.routes");
 const recruiterRoutes = require("./routes/recruiter.routes");
 const soloEntrepreneurRoutes = require("./routes/soloEntrepreneur.routes");
+const dashboardRoutes = require("./routes/dashboard.routes");
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/founders", founderRoutes);
 app.use("/api/recruiters", recruiterRoutes);
 app.use("/api/solo-entrepreneurs", soloEntrepreneurRoutes);
+app.use("/api", dashboardRoutes);
 
 app.get("/", (req, res) => res.send("Welcome to Softpire!"));
 
